8d87aa13a84913a3a64d5dfaabf0a399a3335504
Jahmar-James <Jahmar.d.james@ryerson.ca>
2023-08-13 15:19:06 -0400

    project_init

---
+++ app/main.py
@@ -0,0 +1,8 @@
+# main.py is the entry point for the application
+# Streamlit will run this file when you run the command streamlit run main.py
+
+def main():
+    print("Hello World!")
+    
+if __name__ == "__main__":
+    main()
